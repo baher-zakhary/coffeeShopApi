39b37306ec42c76d55728cbd3fde514c7a9a9d03
Baher Zakhary <baher.zakharyy@gmail.com>
2021-12-04 18:54:24 +0200

    - Implement Model classes with data annotations

---
+++ coffeeShopApi/Models/Drink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coffeeShopApi.Models
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue)]
+        public float Price { get; set; }
+
+        [Required]
+        [StringLength(600)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Category { get; set; }
+    }
+}
---
+++ coffeeShopApi/Models/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coffeeShopApi.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [Phone]
+        public int Phone { get; set; }
+        
+        [Required]
+        public int TotalPeople { get; set; }
+        
+        [Required]
+        public DateTime Date { get; set; }
+        
+        [Required]
+        [DataType(DataType.Time)]
+        public string Time { get; set; }
+    }
+}
