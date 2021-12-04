7947ff040ab98a73de0f66156c75f925349fda65
Baher Zakhary <baher.zakharyy@gmail.com>
2021-12-04 19:39:46 +0200

    - Setup DbContext and connection string

---
+++ coffeeShopApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using coffeeShopApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coffeeShopApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Drink> Drinks { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
